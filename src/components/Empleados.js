e811992b1a7fc1bf2099234d8c2e3fece8a1cf65
CosmeNicolas <nicomas7590@gmail.com>
2024-01-05 17:28:30 -0300

    cambie los nombres de los avatares

---
+++ src/components/Empleados.js
@@ -0,0 +1,11 @@
+  const Empleado = [
+    { id: 1, fullName: "Laya Dueñas", title: "CEO", department: "Business", pic: "avataaars1.png" },
+    { id: 2, fullName: "Astryd Vallés", title: "CMO", department: "Marketing", pic: "avataaars2.png" },
+    { id: 3, fullName: "Shantell Meza", title: "CFO", department: "Business", pic: "avataaars3.png" },
+    { id: 4, fullName: "Sergio Ocampo", title: "CTO", department: "Engineering", pic: "avataaars4.png" },
+    { id: 5, fullName: "Ares Jiménez", title: "Art Director", department: "Marketing", pic: "avataaars5.png" },
+    { id: 6, fullName: "Marta Pérez", title: "Frontend Dev", department: "Engineering", pic: "avataaars6.png" },
+    { id: 7, fullName: "Ellen Balderas", title: "Digital Strategist", department: "Marketing", pic: "avataaars7.png" },
+    { id: 8, fullName: "Cynthia Valentín", title: "Backend Dev", department: "Engineering", pic: "avataaars8.png" },
+    { id: 9, fullName: "Bernard Jung", title: "DevOps Engineer", department: "Engineering", pic: "avataaars9.png" },
+  ]
